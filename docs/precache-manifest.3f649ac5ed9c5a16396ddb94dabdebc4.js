fd56165635cb5d4e6fdbac0a0c66d9b08e158096
Akash Lodha <akashlodha2@gmail.com>
2020-04-28 01:49:16 -0700

    Adding docs for github pages

---
+++ docs/precache-manifest.3f649ac5ed9c5a16396ddb94dabdebc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd22841634c069e73f03fc23630f01a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ea3db46ddd3a1a4a49a",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "5c76c959552d85427300",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ea3db46ddd3a1a4a49a",
+    "url": "/static/js/2.39a12acd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39a12acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c76c959552d85427300",
+    "url": "/static/js/main.cd17dc2e.chunk.js"
+  },
+  {
+    "revision": "429b012a069fc567e7f4",
+    "url": "/static/js/runtime-main.c670b3c9.js"
+  }
+]);
---
+++ docs/precache-manifest.be9a2ed755b332d3f08054833068e4ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c2202cb1de18b80e4f656539fbba0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ea3db46ddd3a1a4a49a",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "99cadac341bed0204dbe",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ea3db46ddd3a1a4a49a",
+    "url": "/static/js/2.39a12acd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39a12acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cadac341bed0204dbe",
+    "url": "/static/js/main.3ccaeab1.chunk.js"
+  },
+  {
+    "revision": "429b012a069fc567e7f4",
+    "url": "/static/js/runtime-main.c670b3c9.js"
+  }
+]);
